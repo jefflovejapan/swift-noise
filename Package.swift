da425fc54b4c25ee1ee38ec6ded1ad085c3424da
Jeff Blagdon <jeffblagdon@gmail.com>
2024-05-27 18:57:59 -0400

    Update Package.swift
    Bump PNG dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: 
     [
-        .package(url: "https://github.com/kelvin13/swift-png", .upToNextMinor(from: "4.0.1"))
+        .package(url: "https://github.com/kelvin13/swift-png", .upToNextMinor(from: "4.4.1"))
     ],
     targets: 
     [
